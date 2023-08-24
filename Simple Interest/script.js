a0a3fac390271db4bf9bb8b5bd754ce7d9bebf80
Kzsanjeet <127291186+Kzsanjeet@users.noreply.github.com>
2023-08-24 22:35:02 +0545

    added simple interest calculator

---
+++ Simple Interest/script.js
@@ -0,0 +1,17 @@
+function calculate(){
+    const p=document.getElementById("principal").value
+    const r=document.getElementById("rate").value
+    const t=document.getElementById("years").value
+    const result=document.getElementById("result")
+    if(p==" " || p==0){
+        alert("Please enter the value")
+    }else{
+
+        interest=(p*t*r)/100
+        result.innerHTML=`The principal is ${p} and rate is ${r} and years is ${t} and the interest is ${interest}`
+    }
+}
+function changing(){
+    const rateval=document.getElementById("rate").value
+    document.getElementById("rate--span").innerText=rateval
+}
